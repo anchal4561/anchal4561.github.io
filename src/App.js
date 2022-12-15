b0b2eddc9421cd58eb81a41c38991a514fdc3090
anchal4561 <anchalagrawal37@gmail.com>
2022-12-15 12:10:58 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import NavBar from "./Components/Navbar"
+import Banner from "./Components/Banner"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Portfolio</h1>
+      <h1>HIIIIIIIIIIIIIIIIIIIIIIII</h1>
+      <NavBar/>
+      <Banner/>
     </div>
   );
 }
